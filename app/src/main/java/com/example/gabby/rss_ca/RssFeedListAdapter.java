83bbb806ce958c6615ca57cefd5a69a19a00b843
Gabby202 <gabbu202@gmail.com>
2019-02-06 20:14:37 +0000

    first commit

---
+++ app/src/main/java/com/example/gabby/rss_ca/RssFeedListAdapter.java
@@ -0,0 +1,63 @@
+package com.example.gabby.rss_ca;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class RssFeedListAdapter
+        extends RecyclerView.Adapter<RssFeedListAdapter.FeedModelViewHolder>{
+
+    private List<RssFeedModel> mRssFeedModels;
+    private Layout parentLayout;
+    public static class FeedModelViewHolder extends RecyclerView.ViewHolder {
+        private View rssFeedView;
+
+        public FeedModelViewHolder(View v) {
+            super(v);
+            rssFeedView = v;
+        }
+    }
+
+    public RssFeedListAdapter(List<RssFeedModel> rssFeedModels) {
+        mRssFeedModels = rssFeedModels;
+    }
+
+    @Override
+    public FeedModelViewHolder onCreateViewHolder(ViewGroup parent, int type) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_rss_feed, parent, false);
+        FeedModelViewHolder holder = new FeedModelViewHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(final FeedModelViewHolder holder, int position) {
+        final RssFeedModel rssFeedModel = mRssFeedModels.get(position);
+        ((TextView)holder.rssFeedView.findViewById(R.id.titleText)).setText(rssFeedModel.title);
+        ((TextView)holder.rssFeedView.findViewById(R.id.descriptionText)).setText(rssFeedModel.description);
+        ((TextView)holder.rssFeedView.findViewById(R.id.linkText)).setText(rssFeedModel.link);
+        holder.rssFeedView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(rssFeedModel.link));
+                view.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRssFeedModels.size();
+    }
+}
+
